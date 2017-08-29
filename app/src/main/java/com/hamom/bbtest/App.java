84f5289c51e8d419bce07093efaea6b38530c415
hamom <hamomel@gmail.com>
2017-08-30 00:20:02 +0400

    main dependencies added, pacage structure created

---
+++ app/src/main/java/com/hamom/bbtest/App.java
@@ -0,0 +1,10 @@
+package com.hamom.bbtest;
+
+import android.app.Application;
+
+/**
+ * Created by hamom on 30.08.17.
+ */
+
+public class App extends Application {
+}
---
+++ app/src/main/java/com/hamom/bbtest/ui/activities/MainActivity.java
@@ -0,0 +1,38 @@
+package com.hamom.bbtest.ui.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import com.hamom.bbtest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    initToolbar();
+    initFAB();
+  }
+
+  private void initToolbar() {
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+  }
+
+  private void initFAB() {
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null)
+            .show();
+      }
+    });
+  }
+}
